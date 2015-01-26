1f87d19e27ad83babc0238ac6a6bbce6884a6a87
ferdi <ferdi554@yahoo.co.id>
2015-01-26 14:28:48 +0700

    tes

---
+++ daftar.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Klinik | Daftar Akun</title>
+	<link rel="stylesheet" type="text/css" href="styledaftar.css">
+</head>
+<body>
+	<div id='wrapper'>
+		<center><img src='img/logodaftar.png'><br>
+		</center>
+		<font class='k' style='float:right;display:inline-block;margin-top:-20px;'>Guest</font>
+		<br>
+		<hr>
+		<font class='k'> Daftar Klinik Ferdi</font>
+		<br><br><br><br><br>
+		<center><font class='k'> Anda belum login ke akun ATM-Ferdi<br>Silahkan login terlebih dahulu </font><br><br>
+			<a href="p/loginatm.php?redirect to daftarpasien.php"> <img src="img/login.png" height='100'> </a>
+			<br><br><br>
+			<font class='sk'> Klinik <span class='yellow'> tidak akan </span>menyimpan kata sandi akun atm anda </font>
+		</center>
+	</div>
+</body>
+</html>
